283aa430624b11f8aeb939a0187c7c764111c323
Srikanth <srikanth.akurathi@gmail.com>
2017-01-20 18:35:11 +0530

    Angular Cli project Intial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  name = '';
+  public radioModel: string = 'Left';
+  public isCollapsed:boolean = false;
+  public collapse:boolean = false;
+  public openSignUpBlock:boolean = false;
+
+  public collapsed(event:any):void {
+    console.log(event);
+  }
+
+  public expanded(event:any):void {
+    console.log(event);
+  }
+
+  public nullCheck(element):boolean {
+    if(element !== undefined || element !== null){
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+  public navBarClick():void {
+    if(event !== undefined || event !== null){
+      /*if(event.target.id === 'signUp' || event.target.id === 'createAccount') {
+        this.openSignUpBlock = true;
+      }else if(event.target.id === 'login') {
+        this.openSignUpBlock = false;
+      }*/
+    }
+  }
 }
